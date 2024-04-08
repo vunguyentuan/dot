4fc4c23c7a5b1636e65c75ee2201e9acdc770298
Vu Nguyen <tuanvu.vn007@gmail.com>
2024-04-08 22:17:52 +0700

    add hypr dotfiles

---
+++ dotfiles/hypr/scripts/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+hyprctl dispatch exec [workspace 1 silent] alacritty
+hyprctl dispatch exec [workspace 2 silent] google-chrome-stable
+hyprctl dispatch exec [workspace 3 silent] slack
